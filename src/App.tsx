5839c3142b93b993f1f879c19c8b280dcb94db3e
atersolis <atersolis@atersolis.net>
2019-10-09 23:23:06 +0200

    Adding message with link

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import * as React from "react";
 import CounterApp from "./counter/Counter";
 import TodoListApp from "./todo/TodoList";
-import { useStore } from "erudx/hooks";
-import { CollectionStore, record } from "erudx/stores";
 import "./app";
 
 const App = () => {
-	let collection = useStore(new CollectionStore([0, 1]));
-
 	return (
 		<div>
-			<h1>Counter example</h1>
+			<h1>Erudx examples</h1>
+			This page showcases examples written using <a href="https://github.com/atersolis/erudx">erudx</a>. <br/>
+			The source code of those examples can be found <a href="https://github.com/atersolis/erudx-examples">here</a>.
+			<div class="separator"></div>
+			<h2>Counter example</h2>
 			<CounterApp />
 			<div class="separator"></div>
-			<h1>Todo List example</h1>
+			<h2>Todo List example</h2>
 			<TodoListApp />
-			<div class="separator"></div>
-			<h1>Collection</h1>
-			{collection.map((n, i) => (
-				<span key={i}>{n}</span>
-			))}
 		</div>
 	);
 };
